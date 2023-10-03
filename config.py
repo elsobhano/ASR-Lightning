a907cf5805cfd3778927c3914aa6d79f7a9021d2
elsobhano <sobhan23578@gmail.com>
2023-10-03 13:37:43 +0330

    Creating configs

---
+++ config.py
@@ -0,0 +1,19 @@
+# Training hyperparameters
+BATCH_SIZE = 16
+save_model_path="./SAVED_MODELS/best_main_GAGNet.pt",
+step_show=3760
+n_epoch=50
+
+
+# Dataset
+SAMPLE_RATE = 16000
+MAX_LENGTH = SAMPLE_RATE * 8
+N_FFT = 400
+HOP_LENGTH = 160
+base_path = "../Data"
+
+
+# Compute related
+ACCELERATOR = "gpu"
+DEVICES = [0]
+PRECISION = 16
